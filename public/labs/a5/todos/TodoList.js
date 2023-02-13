be1cd7b15fdcc98e8cdeefe4fe687cd0708ff1e2
Jin <runninginair>
2023-02-13 00:51:52 -0800

    a5 JavaScript sp23

---
+++ public/labs/a5/todos/TodoList.js
@@ -0,0 +1,8 @@
+const TodoList = () => {
+    return(`<ul>
+                <li>Buy milk</li>
+                <li>Pickup the kids</li>
+                <li>Walk the dog</li>
+            </ul>`);
+    }
+    export default TodoList;
