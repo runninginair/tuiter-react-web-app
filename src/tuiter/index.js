e5b68f91e1180b4f5d2f888d1676f01af23ce72e
Jin <runninginair>
2023-02-21 12:33:56 -0800

    a6 React.js sp23

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import Nav from "../nav";
+import NavigationSidebar from './navigation-sidebar';
+// import WhoToFollowListItem from "./who-to-follow-list/who-to-follow-list-item";
+import WhoToFollowList from './who-to-follow-list/index';
 
 function Tuiter() {
     return (
         <div>
             <Nav />
+            <NavigationSidebar active = "home"/>
+            <WhoToFollowList/>
             <h1>Tuiter</h1>
+
         </div>
     );
 }
