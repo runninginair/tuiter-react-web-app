53af19962245b6b125eac6cf5a17bb128312fd4f
runninginair <runninginair@gmail.com>
2023-03-30 14:32:24 -0700

    url updated

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -4,7 +4,7 @@ import axios from 'axios';                              // import axios
 // const TUITS_API = 'https://tuiter-node-server-app-cs1234-sp23.onrender.com/api/tuits';
 
 // const TUITS_API = 'http://localhost:4000/api/tuits';    // location of HTTP services
-const TUITS_API = "https://tuiter-node-server-app-eetw.onrender.com/api"
+const TUITS_API = "https://tuiter-node-server-app-eetw.onrender.com/api/tuits";
 
 
 export const createTuit = async (tuit) => {
